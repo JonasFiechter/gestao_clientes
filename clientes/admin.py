9b4c9204a07682459388f927ce1d88cbb41c1c22
JonasFiechter <jonasrfiechter@gmail.com>
2022-09-23 14:34:13 -0300

    list_display, list_filter and fieldsets

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Person, Documento, Venda, Produto
 
+class PersonAdmin(admin.ModelAdmin):
+    # fields = ('first_name', 'last_name', 'salary', 'bio', 'photo')
+    fieldsets = (
+        ('Personal data:', {'fields': ('first_name', 'last_name')}),
+        ('Other data:', {'fields': ('age', 'salary', 'photo')}),
+    )
+    list_display = ('first_name', 'last_name', 'salary', 'bio', 'photo')
+    list_filter = ('first_name')
 
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Documento)
 admin.site.register(Venda)
 admin.site.register(Produto)
